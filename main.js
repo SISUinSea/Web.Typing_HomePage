786166653e1f535f2456ce32c3500a42fe09b814
SISU <atgsisu@gmail.com>
2022-12-30 20:45:06 +0900

    none

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-var cursor = document.getElementById('dynamic');
-var index = 0;
+let cursor = document.getElementById('dynamic');
+let index = 0;
 
 function blink(){
     cursor.classList.toggle('active');
 }
 
-var stringArray = ["Vanitas Vanitatum", "et Omnia Vanitas"];
-var selectedString = stringArray[index++%2];
-var selectedCharArray = selectedString.split("");
-console.log(selectedString);
+let stringArray = ["Vanitas Vanitatum", "et Omnia Vanitas"];
+
+function selectingCharArray(){
+    let selectedString = stringArray[(index++)%2];
+    let selectedCharArray = selectedString.split("");
+    console.log(selectedCharArray);
+    return selectedCharArray;
+}
 
 function typing(givenCharArray){
     if(givenCharArray.length>0){
         cursor.textContent += givenCharArray.shift();
-        setTimeout(typing, 300);
+        setTimeout(
+            typing(givenCharArray)
+        , 300);
     }
     else {
-
+        cursor.textContent = "";
+        setTimeout(typing(selectingCharArray()), 3000);
     }
 }
 
 setInterval(blink, 500);
-typing(selectedCharArray);
+typing(selectingCharArray());
 
